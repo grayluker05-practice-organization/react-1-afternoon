610fc398b1e5012612774ad9fb6cce9168507492
grayluker05 <graysonluker@gmail.com>
2019-11-15 16:07:29 -0700

    finished filterobj

--- src/components/Topics/FilterObject.js
+++ src/components/Topics/FilterObject.js
@@ -1,9 +1,63 @@
 import React, {Component} from 'react';
 
 class FilterObject extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            family: [
+                {
+                    name: 'Grayson',
+                    age: 19,
+                    hairColor: 'blonde'
+                },
+                {
+                    name: 'Cory',
+                    jobTitle: 'entreprenuer',
+                    hobby: 'biking'
+                },
+                {
+                    name: 'Natalie',
+                    siblings: true
+                }
+            ],
+            userInput: '',
+            filteredFamily: []
+        }
+    }
+
+    handleChange = (val) => {
+        this.setState({
+            userInput: val
+        })
+    }
+
+    filterFamily = (prop) => {
+        let lukers = this.state.family;
+        let filteredLukers = [];
+
+        for(let i = 0; i < lukers.length; i++){
+            if(lukers[i].hasOwnProperty(prop)){
+                filteredLukers.push(lukers[i])
+            }
+        }
+
+        this.setState({
+            filteredFamily: filteredLukers
+        })
+    }
+
     render(){
         return (
-            <p>FilterObject Component</p>
+            <div className="puzzleBox filterObjectPB">
+                <h4>Filter Object</h4>
+                <span className="puzzleText">Orignial: {JSON.stringify(this.state.family, null, 10)}</span>
+                <input 
+                className="inputLine"
+                onChange={(e) => this.handleChange(e.target.value)}/>
+                <button className="confirmationButton" onClick={() => this.filterFamily(this.state.userInput)}>Filter</button>
+                <span className="resultsBox filterObjectRB">New: {JSON.stringify(this.state.filteredFamily, null, 10)}</span>
+            </div>
         )
     }
 }
